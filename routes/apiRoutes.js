3cece8af8c33c56170c7cae763a2985f1269073d
jbxamora <jbxamora@icloud.com>
2023-02-01 20:10:20 -0800

    updated apiroutes for error handeling and correct callbacks

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,48 +1,91 @@
-const app = require("express").Router();
+// Dependencies
+const express = require("express");
 const fs = require("fs");
-const db = require("../db/db.json");
+const path = require("path");
+const uuidv4 = reqiure("uuid/v4");
+
+const app = require("express").Router();
+const dbPath = path.join(DB, "..", "db", "db.json");
 
 app.get('/notes', (req, res) => {
-    db = JSON.parse(fs.readFileSync('./db/db.json', 'utf-8'))
+   fs.readFile(dbPath, "utf-8", (err,data) => {
+    if (err) {
+        return res.status(500).send("Error reading database");
+    };
 
+    let db;
+    try {
+        db = JSON.parse(data);
+    } catch (err) {
+        return res.status(500).send("Error parsing database");
+    }
     res.json(db);
+   });
 });
 
 app.post('/notes', (req, res) => {
-    let newNote = {
-        id: Math.floor(Math.random() * 1000),
-        title: req.body.title,
-        text: req.body.text
-    }
+    const title = req.body.title;
+    const text = req.body.text;
 
-    db.push(newNote);
-    fs.writeFileSync('./db/db.json', JSON.stringify(db), (err, res) => {
-        if(err) throw err;
+    if (!title || !text) {
+        return res.status(400).send("Title and text are required fields");
+    }
+    
+    fs.writeFileSync(dbPath, "utf-8", (err, data) => {
+        if (err) {
+            return res.status(500).send("Error reading database");
+        }
+
+        let db;
+        try {
+            db = JSON.parse(data);
+        } catch (err) {
+            return res.status(500).send("Error parsing database");
+        }
+
+        const newNote = {
+            id: uuidv4(),
+            title: title,
+            text: text,
+        };
+        db.push(newNote);
+
+        fs.writeFile(dbPath, JSON.stringify(db), (err) => {
+            if (err) {
+                return res.status(500).send("Error writing database");
+            }
+            res.json(db);
+        });
     });
-    res.json(db);
-})
+});
 
 app.delete('/notes/:id', (req, res) => {
-    let db = require('../db/db.json');
-
-    let notesToKeep = [];
-
-    for(let i = 0; i < db.length; i++) {
-        console.log(req.params.id)
-        console.log(db[i]);
-
-        if (parseInt(db[i].id) !== parseInt(req.params.id)) {
-            notesToKeep.push(db[i]);
+    fs.readFile(dbPath, "utf-8", (err,data) => {
+        if (err) {
+            return.res.status(500).send("Error reading database");
         }
-    }
 
-    console.log(notesToKeep);
+        let db;
+        try {
+            db = JSON.parse(data);
+        } catch (err) {
+            return.res.status(500).send("Error parsing database");
+        }
 
-    db = notesToKeep;
-    fs.writeFileSync('./db/db.json', JSON.stringify(db), (err, res) => {
-        if(err) throw err;
+        const id = req.params.id;
+        const notesToKeep = db.filter((note) => note.id !== id);
+
+        if (notesToKeep.length === db.length) {
+            return res.status(404).send("Note not found");
+        }
+
+        fs.writeFile(dbPath, JSON.stringify(notesToKeep), (err) => {
+            if (err) {
+                return res.status(500).send("Error writing database");
+            }
+            res.json(notesToKeep);
+        });
     });
-    res.json(db);
 });
 
 module.exports = app;
